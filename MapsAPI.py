dc1bc5f10792f32970c2b36f81c284bc19618f8d
Nesya <angerasimov04@outlook.com>
2021-02-10 19:18:11 +0300

    homework. №4

--- MapsAPI.py
+++ MapsAPI.py
@@ -12,13 +12,41 @@ delta = '0.002'
 api_server = "http://static-maps.yandex.ru/1.x/"
 
 lon, lat = request[0], request[1]
+mode = 'map'
+
+
+class Button:
+    def __init__(self, name, x, y, color, mode):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.mode = mode
+        self.surface = myfont.render(name, False, (0, 0, 0))
+
+    def get_width(self):
+        return self.surface.get_width()
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color,
+                         (self.x, self.y, self.surface.get_width(), self.surface.get_height()))
+        screen.blit(self.surface, (self.x, self.y))
+
+    def click(self, pos):
+        global mode
+        x = pos[0]
+        y = pos[1]
+        if self.x <= x < self.x + self.surface.get_width() and self.y <= y < self.y + self.surface.get_height():
+            mode = self.mode
+            load_arena()
+            return True
+        return False
 
 
 def load_arena():
     params = {
         "ll": ",".join([lon, lat]),
         "spn": ",".join([delta, delta]),
-        "l": "map"
+        "l": mode
     }
     response = requests.get(api_server, params=params)
     if not response:
@@ -29,9 +57,25 @@ def load_arena():
         file.write(response.content)
     image = pygame.image.load(map_file)
     screen.blit(pygame.image.load(map_file), (0, 0))
+    for button in buttons:
+        button.draw()
     pygame.display.flip()
     return True
 
+
+pygame.font.init()
+myfont = pygame.font.SysFont('Arial', 15)
+
+buttons = []
+x = 0
+map = Button('Схема', x, 0, pygame.Color('red'), 'map')
+buttons.append(map)
+x += map.get_width()
+sat = Button('Спутник', x, 0, pygame.Color('blue'), 'sat')
+buttons.append(sat)
+x += sat.get_width()
+hybrid = Button('Гибрид', x, 0, pygame.Color('green'), 'sat,skl')
+buttons.append(hybrid)
 
 pygame.init()
 pygame.display.set_caption('AAmaps')
@@ -42,6 +86,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            for button in buttons:
+                if button.click(pos):
+                    break
         # !!!Это делал Артём!!!
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             time = float(lat)
